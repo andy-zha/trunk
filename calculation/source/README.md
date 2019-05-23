c838a9d938f3fe964fba7f12e0a8df7047a92a01
andy <andy@localhost.localdomain>
2019-05-23 23:06:36 +0800

    source

---
+++ calculation/source/README.md
@@ -0,0 +1,9 @@
+#编译
+```
+rm -rf build
+mkdir build
+cd build 
+cmake -DCMAKE_BUILD_RPATH=/opt/shark ..
+make
+
+```
---
+++ calculation/source/src/main.cpp
@@ -0,0 +1,10 @@
+#include "StrProc.h"
+
+int main()
+{
+    std::string strone = "abc";
+	std::string strtwo = "abe";
+	float t = StrProc::GetStrMatchDegree(strone, strtwo);
+	printf("%f\n", t);
+	return 0;
+}
